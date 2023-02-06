5f981a2170ab8312db122d344e950832b3f5136b
Imransarkerweb <imransarker.web@gmail.com>
2023-02-06 22:45:03 +0600

    problem 4 and 5 solved

--- assignment.js
+++ assignment.js
@@ -64,3 +64,61 @@ function isLGSeven(number) {
 console.log(isLGSeven(6))
 console.log(isLGSeven(66))
 console.log(isLGSeven('66'))
+
+
+// problem 04
+/*
+findingBadData() function will take an array of number.
+check the number valid or not and count the bad data
+return the bad data.
+*/
+function findingBadData(arrayOfNumber) {
+    if (Array.isArray(arrayOfNumber)) {
+        let badData = 0;
+        for (let i = 0; i < arrayOfNumber.length; i++) {
+            if (typeof arrayOfNumber[i] === 'number') {
+                if (arrayOfNumber[i] < 0) {
+                    badData += 1;
+                }
+            }else{
+                return 'Please put data as number in this array'
+            }
+            
+        } return badData;
+    } else {
+        return 'Please input an array of number'
+    }
+}
+console.log(findingBadData([-4, -9, -5, -33, -55]))
+console.log(findingBadData(['this is just test', 'imarna.', 'this']))
+console.log(findingBadData('oop is ther'))
+
+// problem 5
+/*
+gemsToDiamond() function takes input three friend gems as number.
+then multiply 21 with first friend gems
+then multiply 32 with second friend gems
+then multiply 43 with third friend gems
+count the total gems after multiplying.
+finally if the total gems will grter than or equal to 2000 then deduct 2000 and return the deducted vaule as number.
+else return only total gems
+*/
+function gemsToDiamond(firstFriendGems, secondFriendGems, thirdFriendGems) {
+    if (typeof firstFriendGems === 'number' && typeof secondFriendGems === 'number' && typeof thirdFriendGems === 'number') {
+        const friend1Gems = firstFriendGems * 21;
+        const friend2Gems = secondFriendGems * 32;
+        const friend3Gems = thirdFriendGems * 43;
+        const totalGems = friend1Gems + friend2Gems + friend3Gems;
+        if (totalGems >= 1000 * 2) {
+            return totalGems - 2000;
+        } else {
+            return totalGems;
+        }
+    } else {
+        return 'Please input gems as number.'
+    }
+}
+console.log(gemsToDiamond(20, 200, 50))
+console.log(gemsToDiamond(1, 1, 1))
+console.log(gemsToDiamond(100, 5, 1))
+console.log(gemsToDiamond(20, 2, '25')) 
