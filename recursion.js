22eebf30330ec812679298975784f38c4001e102
Imransarkerweb <imransarker.web@gmail.com>
2023-02-07 22:02:16 +0600

    recursion code same as loop

--- recursion.js
+++ recursion.js
@@ -12,3 +12,23 @@ function sum(number){
 }
 
 console.log(sum(5))
+
+
+// factorial with recursion
+
+function factorial(number){
+    if(number == 1){
+        return 1
+    }
+    return number * factorial(number-1) //recursive call
+}
+
+console.log(factorial(5))
+
+
+//above same code using loop
+let factorials = 1
+for( let i = 5; i>=1; i--){
+     factorials = factorials*i
+}
+console.log(factorials)
