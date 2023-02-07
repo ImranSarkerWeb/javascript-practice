7447831d4639c2e3419d3192e12c9dd1a623dfdc
Imransarkerweb <imransarker.web@gmail.com>
2023-02-07 21:47:41 +0600

    definition added

--- recursion.js
+++ recursion.js
@@ -1,3 +1,9 @@
+/* 
+A function that calls itself is known as recursive function and the approach is called rucursion.
+
+Recursion makes the code clean, simpler and shorter.
+*/
+
 function sum(number){
     if(number == 1){
         return 1;
