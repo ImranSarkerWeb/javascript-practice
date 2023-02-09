3746c62c58f9cb4455d59a5d4f6b3c060940001b
Imransarkerweb <imransarker.web@gmail.com>
2023-02-09 23:01:02 +0600

    dom selector

---
+++ dom.js
@@ -0,0 +1,48 @@
+//select element
+
+//method:1 by getElementsByTagName
+
+const main = document.getElementsByTagName('main')
+console.log(main)
+
+//method: 02 by getElementById
+
+const section1 = document.getElementById("section-1");
+console.log(section1)
+
+//method: 03  by getElementsByClassName
+
+const section2 = document.getElementsByClassName("section-2");
+console.log(section2)
+
+//method: 04 by querySelector and querySelectorAll
+
+const section3 = document.querySelector('.section-3')
+console.log(section3) 
+
+const allLi = document.querySelectorAll('li')
+console.log(allLi)
+
+const newSection = document.createElement('section');
+newSection.innerHTML = `<ul><li>hi from dom</i></ul>`
+
+console.log(newSection)
+
+for(let li of allLi){
+    li.style.color = 'coral';
+}
+
+const addItem = document.getElementById("addItem");
+const item = document.getElementById("item")
+const positionToAdd = document.getElementById("parent");
+console.log(positionToAdd)
+
+
+
+function addNewItem (){
+    //const text = item.value;
+    // let things = '<li>' + '+ text +' + '</li>'
+    // positionToAdd.appendChild(things)
+    let randomColor = Math.floor(444*(Math.random()+1))
+    document.body.style.backgroundColor = '#' + randomColor
+}
