2638e31895252d330d22289d8d1deda26af1eb16
imransarkerweb <imransarker.web@gmail.com>
2023-02-20 19:24:32 +0600

    iheritance learning

---
+++ inheritance.js
@@ -0,0 +1,11 @@
+// inheritance 
+/* 
+    the inheritance model revoves around something called the prototype.
+    Which is often refferred to as an original model from which other forms are developed.
+    In javaScript the prototype is an object that can hold properties to be shared by multiple other objects, and this is the basis of
+    how inheritance works in Javascript.
+*/
+//this is why it's sometimes said tha JavaScript implements a prototype of inheritance model.
+
+
+//lets demonstrat it with a example
