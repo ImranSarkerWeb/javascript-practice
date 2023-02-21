af322aa333bd9a3406249629190e9443e3c361f4
imransarkerweb <imransarker.web@gmail.com>
2023-02-21 16:44:22 +0600

    first problem solved

--- problemFromES6.js
+++ problemFromES6.js
@@ -13,3 +13,28 @@
     method. Then compare the output of question 2 & question 3.
 
 */
+
+//1)
+const oddNums =  [ 1, 3, 5, 7, 9 ]
+
+const evenNums = oddNums.map(num => num+1);
+console.log(evenNums)
+
+//2)
+const numbers = [33, 50, 79, 78, 90, 101, 30 ]
+const divisibleBy10 = numbers.filter(num => num%10==0)
+console.log(divisibleBy10)
+
+//3)
+ const divisibleBy10_2 = numbers.find(num => num%10==0);
+ console.log(divisibleBy10_2)
+
+ /* 
+        the .filter() method return an array of elements where met the condtition is true.
+        the .find() method return only a single element where the first condition met true.
+ */
+
+        
+
+
+
