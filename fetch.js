b6d42b4d426123661e8451c5750d521d2dfac50f
imransarkerweb <imransarker.web@gmail.com>
2023-02-22 22:32:03 +0600

    fetched users data using function

--- fetch.js
+++ fetch.js
@@ -1,5 +1,21 @@
-
+/* 
+    The fetch API is a modern interface that allows you to make HTTP requests to servers from web browsers.
+*/
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
-fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(json))
+fetch(url)//get the data from the url
+    .then(response => response.json()) //converts the data to JSON object
+    .then(json => console.log(json)) // Displays the JSON object
+
+// now collect users data using fetch API and connect with HTML
+
+function getUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => displayData(users))
+}
+
+//we can also connect function
+
+function displayData(data){
+    console.log(data)
+}
