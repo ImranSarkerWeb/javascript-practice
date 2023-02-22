28852719483e51819993abb4ee80da9df664d7bd
imransarkerweb <imransarker.web@gmail.com>
2023-02-22 21:08:11 +0600

    JSON learning started

---
+++ JSON.js
@@ -0,0 +1,12 @@
+// JavaScript Object Notation
+
+/* 
+    We use this data format to exchange data between web application and servers.
+
+    # A JSON Looks like a srigified Version of a regular JS Object.
+
+    # We can create JSON variable like a regular object, but wrapped in single quote.
+*/
+
+const jsonVariable = '{}'
+console.log(typeof jsonVariable) //string
